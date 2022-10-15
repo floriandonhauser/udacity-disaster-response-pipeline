7a2d06faab1abe9ab9048d2381eead73c9183f99
Florian Donhauser <35968719+floriandonhauser@users.noreply.github.com>
2022-10-15 20:40:38 +0200

    Add plotly plots for visualization

--- app/run.py
+++ app/run.py
@@ -1,18 +1,17 @@
 import json
-import plotly
-import pandas as pd
 
+import joblib
+import nltk
+import pandas as pd
+import plotly
+from flask import Flask
+from flask import render_template, request
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
-
-from flask import Flask
-from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
-from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
-
 app = Flask(__name__)
+
 
 def tokenize(text):
     tokens = word_tokenize(text)
@@ -25,51 +24,86 @@ def tokenize(text):
 
     return clean_tokens
 
+
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('Messages', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
-    # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
-    genre_counts = df.groupby('genre').count()['message']
-    genre_names = list(genre_counts.index)
-    
+    # data for messages by genre
+    gen_count = df.groupby('genre').count()['message']
+    gen_per = round(100 * gen_count / gen_count.sum(), 2)
+    gen = list(gen_count.index)
+
+    # data for messages by categories
+    categories = df[df.columns[4:]]
+    cate_counts = (categories.mean() * categories.shape[0]).sort_values(ascending=False)
+    cate_names = list(cate_counts.index)
+
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
     graphs = [
         {
-            'data': [
-                Bar(
-                    x=genre_names,
-                    y=genre_counts
-                )
+            "data": [
+                {
+                    "type": "pie",
+                    "hole": 0.2,
+                    "domain": {
+                        "x": gen_per,
+                        "y": gen
+                    },
+                    "marker": {
+                        "colors": [
+                            "#00B8A9",
+                            "#F6416C",
+                            "#FFDE7D"
+                        ]
+                    },
+                    "textinfo": "label+value",
+                    "labels": gen,
+                    "values": gen_count
+                }
             ],
-
+            "layout": {
+                "title": "Messages by Genre"
+            }
+        },
+        {
+            "data": [
+              {
+                "type": "bar",
+                "x": cate_names,
+                "y": cate_counts,
+                "marker": {
+                  "color": "#00B8A9"}
+                }
+            ],
             'layout': {
-                'title': 'Distribution of Message Genres',
+                'title': 'Distribution of Message Categories',
                 'yaxis': {
                     'title': "Count"
                 },
                 'xaxis': {
-                    'title': "Genre"
+                    'title': "Category"
+                },
+                'margin': {
+                    'b': 200
                 }
-            }
+            },
+            'color': "#00B8A9"
         }
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
@@ -78,7 +112,7 @@ def index():
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
@@ -93,6 +127,7 @@ def go():
 
 
 def main():
+    nltk.download('omw-1.4')
     app.run(host='0.0.0.0', port=3000, debug=True)
 
 
